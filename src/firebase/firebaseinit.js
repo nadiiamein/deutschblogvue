45baaed1ce0a7d60e80a1295412a9727f6814949
nadiiamein <npodufala15@gmail.com>
2021-11-23 18:45:46 +0100

    firebaseinit

---
+++ src/firebase/firebaseinit.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRFMVoWdi8dj0Vv8SClM1lWByF97Qg_eo",
+  authDomain: "deutschblogvue.firebaseapp.com",
+  projectId: "deutschblogvue",
+  storageBucket: "deutschblogvue.appspot.com",
+  messagingSenderId: "238405631903",
+  appId: "1:238405631903:web:5accf916853998f885c7aa"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
